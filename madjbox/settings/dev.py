0bb73b1646e375fb8bc329e03d87185ff84cf910
Ronyonka <rononyonka@gmail.com>
2019-10-11 16:55:18 +0300

    deployment to heroku

--- madjbox/settings/dev.py
+++ madjbox/settings/dev.py
@@ -1,5 +1,6 @@
 from .base import *
-from decouple import config
+from decouple import config, Csv
+import dj_database_url
 
 DATABASES = {
     'default': {
@@ -7,7 +8,11 @@ DATABASES = {
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': '',
+        'HOST': config('DB_HOST'),
         'PORT': ''
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
---
+++ madjbox/settings/prod.py
@@ -0,0 +1,9 @@
+from decouple import config
+import dj_database_url
+from base import *
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
