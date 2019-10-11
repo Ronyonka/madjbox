89a73a12761c63ea39e00913527f92641e6a55e0
Ronyonka <rononyonka@gmail.com>
2019-10-11 15:23:29 +0300

    hid the mapbox access token

--- maps/views.py
+++ maps/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from decouple import config
 
 
 def default_map(request):
-    return render(request, 'default.html', {})
+    mapbox_access_token = config('ACCESS_TOKEN')
+    return render(request, 'default.html', 
+                  { 'mapbox_access_token': mapbox_access_token })
