91fda47d9c05383e27df020511dc71cbe057d8b2
Ronyonka <rononyonka@gmail.com>
2019-10-18 09:53:13 +0300

    added changes to heroku

--- maps/urls.py
+++ maps/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', default_map, name="default"),
-    path('home', home_map, name="home"),
+    path('', home_map, name="home"),
+    path('default', default_map, name="default"),
     path('another', another_map, name="another"),
     path('pics', picture_gallery, name="pics"),
 ]
